97709e23e23fa12b00218c14a2ba78d664dfbdfd
Piotr Dylag <dylag.piotr@gmail.com>
2020-01-09 00:09:39 +0100

    change some visual site. Add cleaning code and newGame button

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,13 +1,17 @@
 var tryCount = 0;
 var guessedNum = 0;
-var randomNum = Math.floor((Math.random() * 10) + 1);
-
-document.getElementById("guess").onclick = function () {
-    var userNum = document.getElementById("user-number").value;
+document.getElementById("newGame").onclick = function() {
+    document.getElementById("user-number").value = "";
+    document.getElementById("message").innerHTML = "";
+    document.getElementById("randNum").innerHTML = "";
+    var randomNum = Math.floor((Math.random() * 10) + 1);
     console.log(randomNum);
-    if(tryCount == 2){
-        alert("Sorry, you reach the try limit! The number was: " + randomNum);
-    }
+
+    document.getElementById("guess").onclick = function () {
+        var userNum = document.getElementById("user-number").value;
+        if (tryCount == 3) {
+            alert("Sorry, you reach the try limit! The number was: " + randomNum);
+        }
         if (randomNum == userNum) {
             document.getElementById("message").innerHTML = "CONGRATS! YOU GUESS MY NUMBER! WHICH WAS: ";
             document.getElementById("randNum").innerHTML = randomNum;
@@ -26,4 +30,5 @@ document.getElementById("guess").onclick = function () {
             document.getElementById("try").innerHTML = "TRY: " + tryCount;
         }
 
+    };
 };
