f9aa19ef88ed03d88fe2a3d01cb4f66e34eb5dfe
Piotr Dylag <dylag.piotr@gmail.com>
2020-01-14 10:44:18 +0100

    Add 3 guess in first try achiv

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,6 @@
 var tryCount = 0;
 var guessedNum = 0;
+var guessRow = 0;
 document.getElementById("newGame").onclick = function() {
     document.getElementById("user-number").value = "";
     document.getElementById("message").innerHTML = "";
@@ -11,11 +12,17 @@ document.getElementById("newGame").onclick = function() {
         var userNum = document.getElementById("user-number").value;
         if (tryCount == 3) {
             alert("Sorry, you reach the try limit! The number was: " + randomNum);
+            guessRow = 0;
+        }
+        if(guessRow == 2){
+            alert("Congrats! You have reach of three guessed on first try. You've get + 10 points!");
+            guessedNum += 10;
         }
         if (randomNum == userNum) {
             document.getElementById("message").innerHTML = "CONGRATS! YOU GUESS MY NUMBER! WHICH WAS: ";
             document.getElementById("randNum").innerHTML = randomNum;
             guessedNum++;
+            guessRow++;
             tryCount = 0;
             document.getElementById("try").innerHTML = "TRY: " + tryCount;
             document.getElementById("score").innerHTML = "SCORE: " + guessedNum;
